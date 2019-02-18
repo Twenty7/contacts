01d29edb1cb0701ffb2a3b536e475767247e43ab
Matt Taylor <mtaylor@illuminateed.com>
2019-02-18 15:25:27 -0800

    Add Contacts Table and API

--- database/migrations/2019_02_18_202552_create_contacts_table.php
+++ database/migrations/2019_02_18_202552_create_contacts_table.php
@@ -15,6 +15,15 @@ class CreateContactsTable extends Migration
     {
         Schema::create('contacts', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email')->unique();
+            $table->string('phone');
+            $table->date('dob');
+            $table->string('address');
+            $table->string('city');
+            $table->string('state');
+            $table->integer('zip');
             $table->timestamps();
         });
     }
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,15 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Contacts
+
+Route::get('contacts', 'ContactController@index');
+
+Route::get('contacts/{contact}', 'ContactController@show');
+
+Route::post('contacts', 'ContactController@store');
+
+Route::put('contacts/{contact}', 'ContactController@update');
+
+Route::delete('contacts/{contact}', 'ContactController@delete');
